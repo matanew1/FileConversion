bcc8dc0311cd0efd408fe79da884dc6120d380a3
Matan.Bardugo <matanew1@gmail.com>
2024-05-30 00:05:53 +0300

    fix all bugs

--- client/src/components/pages/home/Content.jsx
+++ client/src/components/pages/home/Content.jsx
@@ -1,171 +1,141 @@
 import React, { useState } from "react";
-import {
-  Button,
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
-  Paper,
-  Container,
-  Grid,
-} from "@mui/material";
+import { Button, Typography, Grid, Paper, Box } from "@mui/material";
 import { CloudUpload, GetApp } from "@mui/icons-material";
 import axios from "axios";
 import creds from "../../../utils/credentials.json";
 
-const Content = () => {
+const FILE_TYPES = {
+  RAR: ".rar",
+  ZIP: ".zip",
+};
+
+const FileUploadButton = ({ onChange, accept }) => (
+  <label>
+    <input
+      type="file"
+      accept={accept}
+      style={{ display: "none" }}
+      onChange={onChange}
+    />
+    <Button variant="contained" component="span" startIcon={<CloudUpload />}>
+      Upload {accept} File
+    </Button>
+  </label>
+);
+
+const DownloadButton = ({ onClick, fileType, to }) => (
+  <Button variant="contained" onClick={onClick} startIcon={<GetApp />}>
+    Download As {to}
+  </Button>
+);
+
+const FileCard = ({ title, fileType, to }) => {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const downloadFile = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.get(
-        `${creds.baseUrl}/download?_id=${selectedFile._id}`,
-      );
+      const response = await axios.get(`${creds.baseUrl}/download?_id=${selectedFile._id}`);
+  
       if (response.status === 200) {
         const fileName = response.data;
-        window.location.href = `${creds.baseUrl}/download/${fileName}`;
+        const [name, type] = fileName.split(".");
+        const extension = name.includes(".zip") || name.includes(".rar") ? "" : `.${type}`;
+  
+        window.location.href = `${creds.baseUrl}/download/${name}${extension}`;
+      } else {
+        console.error(`Failed to download file: Server responded with status ${response.status}`);
       }
     } catch (error) {
-      console.error(error);
+      console.error(`Failed to download file: ${error.message}`);
     }
   };
 
-  const handleFileChange = (event) => {
+  const handleFileChange = async (event) => {
     event.preventDefault();
     const formData = new FormData();
     formData.append("file", event.target.files[0]);
 
-    if (event.target.files.length > 0)
-      axios
-        .post(`${creds.baseUrl}/upload`, formData, {
+    if (event.target.files.length > 0) {
+      try {
+        const response = await axios.post(`${creds.baseUrl}/upload`, formData, {
           headers: { "Content-Type": "multipart/form-data" },
-        })
-        .then((response) => {
-          setSelectedFile(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
         });
+        setSelectedFile(response.data);
+      } catch (error) {
+        console.error(`Failed to upload file: ${error.message}`);
+        alert(`Failed to upload file: ${error.message}`);
+      }
+    }
   };
 
+  return (
+    <Grid item xs={12} sm={12}>
+      <Paper
+        sx={{
+          background: "transparent",
+          color: "white",
+          padding: "2rem",
+        }}
+      >
+        <Typography variant="h5">{title}</Typography>
+        <br />
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <Box mb={2}>
+              <FileUploadButton onChange={handleFileChange} accept={fileType} />{" "}
+            </Box>
+          </Grid>
+          {selectedFile && (
+            <Grid item xs={12} sm={6}>
+              <Box mb={2}>
+                <DownloadButton
+                  onClick={downloadFile}
+                  fileType={fileType}
+                  to={to}
+                />
+              </Box>
+            </Grid>
+          )}
+          <Grid item xs={12} sm={6}>
+            <Box mb={2}>
+              <Typography variant="h7" color="white">
+                &nbsp;&nbsp;&nbsp;{" "}
+                {selectedFile ? "File: " + selectedFile.originalname : ""}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+};
+
+const Content = () => {
   const cards = [
     {
       title: "CONVERT RAR TO ZIP",
-      content: (
-        <Typography>
-          <label>
-            <input
-              type="file"
-              style={{ display: "none" }}
-              onChange={handleFileChange}
-            />
-            <Button
-              variant="contained"
-              component="span"
-              startIcon={<CloudUpload />}
-            >
-              Upload RAR File
-            </Button>
-            <Typography variant="h7" color="initial">
-              &nbsp;&nbsp;&nbsp; {selectedFile ? selectedFile.originalname : ""}
-            </Typography>
-          </label>
-        </Typography>
-      ),
-      download:
-        selectedFile !== null ? (
-          <Button
-            variant="contained"
-            name={"zip"}
-            onClick={downloadFile}
-            startIcon={<GetApp />}
-          >
-            Download As {"ZIP"}
-          </Button>
-        ) : null,
+      fileType: FILE_TYPES.RAR,
+      to: FILE_TYPES.ZIP,
     },
     {
       title: "CONVERT ZIP TO RAR",
-      content: (
-        <Typography>
-          <label>
-            <input
-              type="file"
-              style={{ display: "none" }}
-              onChange={handleFileChange}
-            />
-            <Button
-              variant="contained"
-              component="span"
-              startIcon={<CloudUpload />}
-            >
-              Upload ZIP File
-            </Button>
-            <Typography variant="h7" color="initial">
-              &nbsp;&nbsp;&nbsp; {selectedFile ? selectedFile.originalname : ""}
-            </Typography>
-          </label>
-        </Typography>
-      ),
-      download:
-        selectedFile !== null ? (
-          <Button
-            variant="contained"
-            name={"rar"}
-            onClick={downloadFile}
-            startIcon={<GetApp />}
-          >
-            Download As {"RAR"}
-          </Button>
-        ) : null,
+      fileType: FILE_TYPES.ZIP,
+      to: FILE_TYPES.RAR,
     },
   ];
 
   return (
-    <Container>
-      <Grid container spacing={2}>
-        {cards.map((card, index) => (
-          <Grid item xs={12} sm={12} key={index}>
-            <Paper
-              elevation={3}
-              sx={{
-                background: "transparent",
-                color: "white",
-                padding: "2rem",
-              }}
-            >
-              <Typography variant="h5">{card.title}</Typography>
-              <br />
-              <Grid item>
-                {Array.isArray(card.content) ? (
-                  <List>
-                    {card.content.map((item, index) => (
-                      <ListItem key={index}>
-                        <ListItemText primary={item} />
-                      </ListItem>
-                    ))}
-                  </List>
-                ) : (
-                  <Grid
-                    item
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                    }}
-                  >
-                    {card.content}
-                    <br />
-                    {card.download}
-                  </Grid>
-                )}
-              </Grid>
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <Grid container spacing={2}>
+      {cards.map((card) => (
+        <FileCard
+          key={card.title}
+          title={card.title}
+          fileType={card.fileType}
+          to={card.to}
+        />
+      ))}
+    </Grid>
   );
 };
 
--- server/services/fileService.js
+++ server/services/fileService.js
@@ -2,83 +2,94 @@ const File = require("../models/file.js");
 const fs = require("fs");
 const path = require("path");
 const zipper = require("zip-local");
+const { exec } = require("child_process");
 const { createExtractorFromFile } = require("node-unrar-js");
+const extract = require("extract-zip");
+
+const FILE_TYPES = {
+  RAR: "rar",
+  ZIP: "zip",
+};
+
+const UPLOADS_DIR = path.join(__dirname, "..", "uploads");
 
 class FileService {
   static uploadFile = async (file) => {
-    try {
-      const fileEntity = new File(file);
-      return await fileEntity.save();
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    const fileEntity = new File(file);
+    return await fileEntity.save();
   };
 
   static unRarFile = async (pathFile, destination) => {
-    try {
-      // Create the extractor with the pathFile information (returns a promise)
-      const extractor = await createExtractorFromFile({
-        filepath: pathFile,
-        targetPath: destination,
-      });
-      // Extract the files
-      [...extractor.extract().files];
-    } catch (err) {
-      // May throw UnrarError, see docs
-      console.error(err);
-    }
+    const extractor = await createExtractorFromFile({
+      filepath: pathFile,
+      targetPath: destination,
+    });
+    [...extractor.extract().files];
   };
 
   static unZipFile = async (pathFile, destination) => {
     try {
+      console.log(pathFile);
+      await extract(pathFile, { dir: destination });
+      console.log("Extraction complete");
     } catch (err) {
-      // May throw UnrarError, see docs
       console.error(err);
     }
   };
 
-  static createZipFile = async (pathFile, fileName) => {
-    try {
-      zipper.sync
-        .zip(pathFile)
-        .compress()
-        .save(path.join(__dirname, "..", `uploads/${fileName}.zip`));
-    } catch (error) {
-      throw new Error(error.message);
+  static createCompressedFile = async (pathFile, fileName, type) => {
+    const filePath = this.getFilePath(fileName, type);
+    
+    // Check if file already exists
+    if (fs.existsSync(filePath)) {
+      console.log(`File ${filePath} already exists. Skipping compression.`);
+      return;
+    }
+  
+    if (type === FILE_TYPES.RAR) {
+      await this.createRarCompressedFile(pathFile, fileName, type);
+    } else if (type === FILE_TYPES.ZIP) {
+      await this.createZipCompressedFile(pathFile, fileName, type);
     }
   };
 
-  static createRarFile = async (pathFile, fileName) => {
-    try {
-      zipper.sync
-        .rar(pathFile)
-        .compress()
-        .save(path.join(__dirname, "..", `uploads/${fileName}.rar`));
-    } catch (error) {
-      throw new Error(error.message);
-    }
+  static createZipCompressedFile = async (pathFile, fileName, type) => {
+    zipper.sync[type](pathFile)
+      .compress()
+      .save(this.getFilePath(fileName, type));
   };
 
-  static downloadFile = async (fileId, fileType) => {
-    try {
-      const file = await File.findById(fileId);
-      const fileName = String(file.originalname).split(".")[0];
-      const fileType = String(file.originalname).split(".")[1];
-      console.log(fileName, fileType);
-      const outputDir = path.join(__dirname, "..", `uploads/${fileName}`);
-      if (fileType === "rar") {
-        await this.unRarFile(file.path, outputDir);
-        await this.createZipFile(outputDir, fileName);
-        return fileName + ".zip";
+  static createRarCompressedFile = async (pathFile, fileName, type) => {
+    exec(
+      `rar a "${this.getFilePath(fileName, type)}" "${pathFile}"`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
       }
-      if (fileType === "zip") {
-        await this.unZipFile(file.path, outputDir);
-        await this.createRarFile(outputDir, fileName);
-        return fileName + ".rar";
-      }
-    } catch (error) {
-      throw new Error(error.message);
+    );
+  };
+
+  static getFilePath = (fileName, type) => {
+    return path.join(UPLOADS_DIR, `${fileName}.${type}`);
+  };
+
+  static downloadFile = async (fileId) => {
+    const file = await File.findById(fileId);
+    const fileName = path.parse(file.originalname).name;
+    const fileType = path.parse(file.originalname).ext.slice(1);
+    const outputDir = path.join(UPLOADS_DIR, fileName);
+
+    if (fileType === FILE_TYPES.RAR) {
+      await this.unRarFile(file.path, outputDir);
+      await this.createCompressedFile(outputDir, fileName, FILE_TYPES.ZIP);
+    } else if (fileType === FILE_TYPES.ZIP) {
+      await this.unZipFile(file.path, outputDir);
+      await this.createCompressedFile(outputDir, fileName, FILE_TYPES.RAR);
     }
+
+    return fileName + (fileType === FILE_TYPES.RAR ? `.${FILE_TYPES.ZIP}` : `.${FILE_TYPES.RAR}`);
   };
 }
 
